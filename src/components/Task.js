996b1e4d99d5be8e328b12fe0d53f7011a92e8b0
xavir <xaviranik.me@gmail.com>
2021-05-17 13:40:35 +0600

    add: react icons

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,7 +1,12 @@
+import { FaTimes } from "react-icons/fa";
+
 const Task = ({ task }) => {
   return (
     <div className="bg-white p-6 mb-4 rounded-md shadow-sm">
-      <h1 className="text-lg font-medium text-gray-600 mb-2">{task.text}</h1>
+      <div className="flex justify-between items-center text-lg font-medium text-gray-600 mb-2">
+        <h1>{task.text}</h1>
+        <FaTimes className="text-red-500" />
+      </div>
       <p className="text-sm text-gray-400">{task.day}</p>
     </div>
   );
